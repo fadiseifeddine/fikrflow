421f9f6ff6a53261203da38f801fb5cc85723097
ojoadmin <admin@ojolure.com>
2023-09-27 08:04:17 -0400

    Add Collab Pointer + User

--- js/fikrcollab.js
+++ js/fikrcollab.js
@@ -20,23 +20,18 @@ socket.on('mousemove', (data) => {
     createPointer(data.sessionId, data.userId, data.x, data.y, data.color);
 });
 
-// Emit your mouse move data to the server
+
+
+// Event listener for mouse move
 document.addEventListener('mousemove', (e) => {
 
-    console.log('Emit mouse move to Server');
-
-
+    // Emit your mouse move data to the server here (if needed)
     const mouseX = e.clientX;
     const mouseY = e.clientY;
     const vuserId = common.getUserId();
-
-    console.log('Emit userId', vuserId);
-    console.log('Emit sessionid ', sessionId);
-
-    // Emit your mouse move data to the server
     socket.emit('mousemove', {
         sessionId: sessionId,
-        userId: vuserId, // Replace with your user ID logic
+        userId: vuserId,
         x: mouseX,
         y: mouseY,
         color: userColor,
@@ -44,28 +39,50 @@ document.addEventListener('mousemove', (e) => {
 });
 
 
+
+
 function createPointer(sessionId, userId, x, y, color) {
     // Check if the pointer should be created for another user (not the current user)
-    console.log(" xyz_userId = ", userId);
-    console.log(" xyz_common.userId = ", common.userId);
-
     if (userId !== common.getUserId()) {
-        console.log("Creating Pointer for user id", userId);
-        console.log("x:", x, "y:", y, "color:", color, "userId:", userId, "sessionId:", sessionId);
+        // Create a custom cursor element
+        const customCursor = document.createElement('div');
+        customCursor.className = 'custom-cursor';
+        customCursor.style.left = x + 'px';
+        customCursor.style.top = y + 'px';
+        document.body.appendChild(customCursor);
 
-        // Create an icon pointer element
-        const pointer = document.createElement('div');
-        pointer.className = 'pointer';
-        pointer.style.backgroundColor = color;
-        pointer.style.left = x + 'px';
-        pointer.style.top = y + 'px';
-        pointer.textContent = userId;
-        document.body.appendChild(pointer);
+        // Create a span element for the userId
+        const userIdElement = document.createElement('span');
+        userIdElement.textContent = userId;
+        userIdElement.style.color = color;
+        customCursor.appendChild(userIdElement);
 
-        // Remove the pointer after a certain time (e.g., 5 seconds)
+        // Create an SVG element for the icon
+        const svgNS = "http://www.w3.org/2000/svg";
+        const icon = document.createElementNS(svgNS, 'svg');
+        icon.setAttributeNS(null, 'viewBox', '0 0 16 16');
+        icon.setAttributeNS(null, 'fill', color);
+        icon.classList.add('bi', 'bi-mouse-pointer-fill');
+
+        // Create a path element for the icon shape
+        const path = document.createElementNS(svgNS, 'path');
+        path.setAttributeNS(null, 'd', 'M4.40701 3.41403C3.94876 3.27925 3.71963 3.21186 3.56363 3.27001C3.42768 3.32069 3.32045 3.42793 3.26977 3.56387C3.21162 3.71988 3.27901 3.949 3.41379 4.40726L7.61969 18.7073C7.74493 19.1332 7.80756 19.3461 7.93395 19.4449C8.04424 19.5312 8.18564 19.5672 8.32377 19.5443C8.48206 19.5181 8.639 19.3611 8.95286 19.0473L11.9999 16.0002L16.4343 20.4345C16.6323 20.6325 16.7313 20.7315 16.8454 20.7686C16.9459 20.8012 17.054 20.8012 17.1545 20.7686C17.2686 20.7315 17.3676 20.6325 17.5656 20.4345L20.4343 17.5659C20.6323 17.3679 20.7313 17.2689 20.7684 17.1547C20.801 17.0543 20.801 16.9461 20.7684 16.8457C20.7313 16.7315 20.6323 16.6325 20.4343 16.4345L15.9999 12.0002L19.047 8.95311C19.3609 8.63924 19.5178 8.48231 19.5441 8.32402C19.567 8.18589 19.5309 8.04448 19.4447 7.93419C19.3458 7.8078 19.1329 7.74518 18.7071 7.61993L4.40701 3.41403Z');
+        icon.appendChild(path);
+
+        customCursor.appendChild(icon);
+
+        // Listen for mousemove events for this user
+        socket.on('mousemove', (data) => {
+            if (data.sessionId === sessionId && data.userId === userId) {
+                // Update the custom cursor position
+                customCursor.style.left = data.x + 'px';
+                customCursor.style.top = data.y + 'px';
+            }
+        });
+
+        // Remove the custom cursor after a certain time (e.g., 5 seconds)
         setTimeout(() => {
-            console.log("Removing Pointer for user id", userId);
-            document.body.removeChild(pointer);
+            document.body.removeChild(customCursor);
         }, 5000);
     }
 }
