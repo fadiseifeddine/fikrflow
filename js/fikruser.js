ff18285798fc8e2d01dc7e9d316e9783fdffc60a
ojoadmin <admin@ojolure.com>
2023-10-03 09:24:44 -0400

    Sign-up / Sing-In Authentication

--- js/fikruser.js
+++ js/fikruser.js
@@ -1,3 +1,5 @@
+import * as common from './common.js';
+
 //Registered Users
 let userId = null;
 let userName = null;
@@ -30,7 +32,9 @@ const selectedUserElement = document.getElementById('selectedUser');
 
 // Add an event listener for the "Register User" button to open the modal
 document.addEventListener("DOMContentLoaded", function() {
-    // Add a click event listener to the "Register User" button
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////// Registration SIGNUP
+    // Add a click event listener to the "Register User" button (MENU SIGN-UP / REGISTER USER)
     const registerUserButton = document.getElementById("registeruser");
     if (registerUserButton) {
         registerUserButton.addEventListener("click", function() {
@@ -46,101 +50,244 @@ document.addEventListener("DOMContentLoaded", function() {
             const registrationModal = new bootstrap.Modal(document.getElementById("registrationModal"));
             registrationModal.show();
         });
+    }
 
 
-        // Add a click event listener to the "Save" button in the modal
-        const saveuserRegistrationButton = document.getElementById("saveUserRegistration");
-        if (saveuserRegistrationButton) {
-            saveuserRegistrationButton.addEventListener("click", (event) => {
+    const userIdElement = document.getElementById("userId");
 
+    // Add a blur event listener to the userIdInput field
+    userIdElement.addEventListener("blur", async() => {
 
+        const userId = userIdElement.value;
 
-                // Clear previous error messages
-                clearFieldErrors();
+        if (!isFieldEmpty(userId)) {
 
-                const userId = document.getElementById('userId').value;
-                const userName = document.getElementById('userName').value;
-                const password = document.getElementById('password').value;
-                const confirmPassword = document.getElementById('confirmPassword').value;
+            // Check if the user already exists
+            const userExists = await checkuser(userId);
+            console.log("userExists", userExists);
 
-                // Validate user inputs
-                if (isFieldEmpty(userId)) {
-                    showFieldError('userId', 'User ID is required.');
-                    return;
-                }
+            if (userExists == "user_found") {
+                // Generate a random 2-digit number
+                const randomTwoDigitNumber = Math.floor(Math.random() * 100);
 
-                if (isFieldEmpty(userName)) {
-                    showFieldError('userName', 'User Name is required.');
-                    return;
-                }
+                // Suggest an alternative username
+                const suggestedUserId = `${userId}${randomTwoDigitNumber}`;
 
-                if (isFieldEmpty(password)) {
-                    showFieldError('password', 'Password is required.');
-                    return;
-                }
+                // Show an error message with the suggestion
+                showFieldError('userId', `User ID "${userId}" is already taken. You can use "${suggestedUserId}" or choose another.`);
+                return;
+            } else {
+                console.log("User Not Found. Registering User =", userId);
+                showFieldError('userId', '', true); // Clear the error
 
-                if (isFieldEmpty(confirmPassword)) {
-                    showFieldError('confirmPassword', 'Confirm Password is required.');
-                    return;
-                }
-
-                if (password !== confirmPassword) {
-                    showFieldError('password', 'Password and confirm password do not match.');
-                    showFieldError('confirmPassword', 'Password and confirm password do not match.');
-                    return;
-                }
-
-                if (!checkPasswordStrength(password)) {
-                    showFieldError('password', 'Password does not meet the strength requirements.');
-                    return;
-                }
-
-                // Your registration logic here
-                // Send the data to your server or perform any necessary actions
-                handleuserRegistrationForm(event);
-                // Close the modal
-                // $('#registrationModal').modal('hide');
-            });
-
-
-
-
-
-
-
-
-
-
+            }
+        } else {
+            showFieldError('userId', 'User ID is required.');
+            return;
         }
+    });
 
+
+    // Add a click event listener to the "Save" button in the modal
+    const saveuserRegistrationButton = document.getElementById("saveUserRegistration");
+    if (saveuserRegistrationButton) {
+        saveuserRegistrationButton.addEventListener("click", async(event) => {
+
+
+
+            // Clear previous error messages
+            const fields = ['userId', 'userName', 'password', 'confirmPassword'];
+
+            clearFieldErrors(fields);
+
+            const userId = document.getElementById('userId').value;
+            const userName = document.getElementById('userName').value;
+            const password = document.getElementById('password').value;
+            const confirmPassword = document.getElementById('confirmPassword').value;
+
+
+            if (!isFieldEmpty(userId)) {
+
+                // Check if the user already exists
+                const userExists = await checkuser(userId);
+                console.log("userExists", userExists);
+
+                if (userExists == "user_found") {
+                    // Generate a random 2-digit number
+                    const randomTwoDigitNumber = Math.floor(Math.random() * 100);
+
+                    // Suggest an alternative username
+                    const suggestedUserId = `${userId}${randomTwoDigitNumber}`;
+
+                    // Show an error message with the suggestion
+                    showFieldError('userId', `User ID "${userId}" is already taken. You can use "${suggestedUserId}" or choose another.`);
+                    return;
+                } else {
+                    console.log("User Not Found. Registering User =", userId);
+                    showFieldError('userId', '', true); // Clear the error
+
+                }
+            } else {
+                showFieldError('userId', 'User ID is required.');
+                return;
+            }
+
+            if (isFieldEmpty(userName)) {
+                showFieldError('userName', 'User Name is required.');
+                return;
+            }
+
+            if (isFieldEmpty(password)) {
+                showFieldError('password', 'Password is required.');
+                return;
+            }
+
+            if (isFieldEmpty(confirmPassword)) {
+                showFieldError('confirmPassword', 'Confirm Password is required.');
+                return;
+            }
+
+            if (password !== confirmPassword) {
+                showFieldError('password', 'Password and confirm password do not match.');
+                showFieldError('confirmPassword', 'Password and confirm password do not match.');
+                return;
+            }
+
+            if (!checkPasswordStrength(password)) {
+                showFieldError('password', 'Password does not meet the strength requirements.');
+                return;
+            }
+
+
+            // Your registration logic here
+            // Send the data to your server or perform any necessary actions
+            handleuserRegistrationForm(event);
+            // Close the modal
+            // $('#registrationModal').modal('hide');
+        });
 
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////// Login
+    // Add a click event listener to the "Login User" button (MENU SIGN-IN / LOGIN USER)
+    const loginUserButton = document.getElementById("loginuser");
+    if (loginUserButton) {
+        loginUserButton.addEventListener("click", function() {
+
+            console.log("Sign-In Clearing the Dialog first....");
+            // Clear the form fields when opening the modal
+            document.getElementById("loginuserid").value = "";
+            document.getElementById("loginpassword").value = "";
+
+            console.log("Sign-In Instatiating the Modal....");
+
+            // Trigger the modal to show
+            const loginModal = new bootstrap.Modal(document.getElementById("loginModal"));
+
+            console.log("Sign-In Showing the Modal....");
+
+            loginModal.show();
+        });
+    }
+
+    // Add an event listener to the "Login" button inside the modal
+    document.getElementById("loginButton").addEventListener("click", async function() {
+        // Your login logic goes here
+
+        console.log("In Login .... ");
+
+        // Clear previous error messages
+        const fields = ['loginuserid', 'loginpassword'];
+        clearFieldErrors(fields);
+
+
+        const userId = document.getElementById('loginuserid').value;
+        const password = document.getElementById('loginpassword').value;
+
+        if (isFieldEmpty(userId)) {
+            showFieldError('loginuserid', 'User ID is required.');
+            return;
+        }
+
+        if (isFieldEmpty(password)) {
+            showFieldError('loginpassword', 'Password is required.');
+            return;
+        }
+
+
+        // Your registration logic here
+        // Send the data to your server or perform any necessary actions
+        handleuserLoginForm(event);
+
+    });
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 });
 
 
 
-// Function to handle the form submission
+// Function to handle the Register User / Sign-Up form submission
 function handleuserRegistrationForm(event) {
     event.preventDefault(); // Prevent the form from submitting and reloading the page
 
     // Get the values from the input fields
-    userId = document.getElementById("userId").value;
-    userName = document.getElementById("userName").value;
+    const userId = document.getElementById("userId").value;
+    const userName = document.getElementById("userName").value;
+    const password = document.getElementById("password").value;
 
-    // Display the collected User ID and User Name
-    const registrationResult = document.getElementById("registrationResult");
-    //console.log(`User ID: ${userId}, User Name: ${userName}`);
-    selectedUserElement.textContent = userId;
-    // fadi here we are reusing the same session when switching from non user to user
 
+
+
+    // Register the user (your registration logic here)
+    // Send the data to your server or perform any necessary actions
+    registeruser(userId, userName, password);
 
 
     // Close the modal
     var myModalEl = document.getElementById('registrationModal');
     var modal = bootstrap.Modal.getInstance(myModalEl); // Returns a Bootstrap modal instance
     modal.hide();
+
+    selectedUserElement.textContent = userId;
+    // fadi here we are reusing the same session when switching from non user to user
+}
+
+
+// Function to handle the Login User / Sign-In form submission
+async function handleuserLoginForm(event) {
+    event.preventDefault(); // Prevent the form from submitting and reloading the page
+
+
+    console.log("handleuserLoginForm ....");
+    // Get the values from the input fields
+    const userId = document.getElementById("loginuserid").value;
+    const password = document.getElementById("loginpassword").value;
+
+
+    // Register the user (your registration logic here)
+    // Send the data to your server or perform any necessary actions
+    const autheticated = await authenticateuser(userId, password);
+    if (autheticated == true) {
+        console.log("autheticated .... True");
+        // Close the modal
+        var myModalEl = document.getElementById('loginModal');
+        var modal = bootstrap.Modal.getInstance(myModalEl); // Returns a Bootstrap modal instance
+        modal.hide();
+
+        selectedUserElement.textContent = userId;
+        // fadi here we are reusing the same session when switching from non user to user
+
+    } else {
+        console.log("autheticated .... false");
+
+        return;
+    }
+
+
+
 }
 
 
@@ -182,18 +329,25 @@ function isFieldEmpty(fieldValue) {
     return fieldValue.trim() === '';
 }
 
-// Function to show an error message for a field
-function showFieldError(fieldId, errorMessage) {
+// Function to show or clear an error message for a field
+function showFieldError(fieldId, errorMessage, clearError = false) {
     const fieldElement = document.getElementById(fieldId);
-    fieldElement.classList.add('is-invalid');
     const errorElement = document.getElementById(`${fieldId}Error`);
-    errorElement.textContent = errorMessage;
-    errorElement.style.display = 'block';
+
+    if (clearError) {
+        fieldElement.classList.remove('is-invalid');
+        errorElement.textContent = '';
+        errorElement.style.display = 'none';
+    } else {
+        fieldElement.classList.add('is-invalid');
+        errorElement.textContent = errorMessage;
+        errorElement.style.display = 'block';
+    }
 }
 
+
 // Function to clear error messages for all fields
-function clearFieldErrors() {
-    const fields = ['userId', 'userName', 'password', 'confirmPassword'];
+function clearFieldErrors(fields) {
     fields.forEach((fieldId) => {
         const fieldElement = document.getElementById(fieldId);
         fieldElement.classList.remove('is-invalid');
@@ -201,6 +355,98 @@ function clearFieldErrors() {
         errorElement.textContent = '';
         errorElement.style.display = 'none';
     });
+}
+
+// Function to check if a user exists
+async function checkuser(userId) {
+    const response = await fetch(`http://localhost:3000/api/checkuser?userId=${userId}`);
+    const data = await response.json();
+    console.log("user exists ? ", data);
+    return data.result;
+}
+
+
+
+async function registeruser(userId, userName, password) {
+    // Check if the user provided a file name
+
+
+    // Proceed with saving using the file name
+    try {
+        const response = await fetch('http://localhost:3000/api/registeruser', {
+            method: 'POST',
+            body: JSON.stringify({
+                userId: userId,
+                userName: userName,
+                password: password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log("response =", response);
+
+        if (response.ok) {
+            //console.log('Drawing saved successfully');
+            common.showMessage('User Registered successfully ...', 2000);
+            return true; // Return true on success
+        } else {
+            console.error('Failed to save drawing');
+            // Handle the error and provide appropriate user feedback
+            return false; // Return false on failure
+        }
+    } catch (error) {
+        console.error('An error occurred:', error);
+        // Handle the error and provide appropriate user feedback
+        return false; // Return false on error
+    }
+
+}
+
+
+async function authenticateuser(userId, password) {
+    console.log("authenticateuser ....");
+
+    // Check if the user already exists
+    const userAuthenticated = await checkUserPassword(userId, password);
+    console.log("userAuthenticated", userAuthenticated);
+
+    if (userAuthenticated.result == "user_authenticated") {
+        console.log("Authenticated  True...");
+        return true;
+
+    } else {
+
+        showFieldError('loginuserid', 'User and/Or Combination Not Valid...');
+        showFieldError('loginpassword', 'User and/Or Combination Not Valid...');
+        return false;
+
+    }
+
+}
+
+// Function to check if a user exists
+// Function to check if a user exists and authenticate
+async function checkUserPassword(loginUserId, loginPassword) {
+    try {
+        const response = await fetch('http://localhost:3000/api/authenticate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                userId: loginUserId,
+                password: loginPassword,
+            }),
+        });
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        return { success: false, error: error.message };
+    }
 }
 
 
